47b76fedbc0602c46bf987267b1bf2f0ab1bd6ad
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2023-08-19 12:28:23 -0500

    Desarrollo 1 Jugador.h
    Se declararon las variables y se generan los getters, setters y el constructor

--- Jugador.h
+++ Jugador.h
@@ -1,3 +1,28 @@
 #ifndef __JUGADOR__H__
 #define __JUGADOR__H__
+#include <iostream>
 
+class Jugador{
+	private:
+		int id;
+		char color[10];
+		char alias[20];
+		int unidades;
+		std::list<Cartas> cartas;
+		char estado[10];
+	public:
+		Jugador();
+		void setId(int id);
+		void setColor(char color[]);
+		void setAlias(char alias[]);
+		void setUnidades(int unidades);
+		void setCartas(std::list<Cartas> cartas);
+		void setEstado(char Estado[]);
+		int getId();
+		char* getColor();
+		char* getAlias();
+		int getUnidades();
+		std::list<Cartas> getCartas();
+		char* getEstado();
+
+}
